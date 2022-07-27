e21c5f1a55c7b2da665a68f20edc6cf624818282
Ivan <vana.antropov@gmail.com>
2022-07-28 01:39:15 +0500

    Second commit

--- exercise1.py
+++ exercise1.py
@@ -10,15 +10,29 @@ def InputValues (text: str):
             number = int(input(f'{text}'))
     return number
 
-def MultiOfNumbers(num: int):
-    multi = 1
-    listOfMulti = []
-    for i in range(num):
-        multi = multi*(i + 1)
-        listOfMulti.append(multi)
-    defString = f'A set of multi of numbers from 1 to {num} is {listOfMulti}'
-    return defString
+# def MultiOfNumbers(num: int):
+#     multi = 1
+#     listOfMulti = []
+#     for i in range(num):
+#         multi = multi*(i + 1)
+#         listOfMulti.append(multi)
+#     defString = f'A set of multi of numbers from 1 to {num} is {listOfMulti}'
+#     return defString
 
+# numberN = InputValues('Enter number N: ')
+# print(MultiOfNumbers(numberN))
+
+# ------------------------------------------------------------------------------
+
+def f(num: int):
+    multi = 1
+    while num>0:
+        multi = num * multi
+        num -= 1
+    return multi
 
 numberN = InputValues('Enter number N: ')
-print(MultiOfNumbers(numberN))
+listOfNumbers = [i+1 for i  in range(numberN)]
+print(listOfNumbers)
+listOfMulti = list(map(f, listOfNumbers))
+print(listOfMulti)
--- exercise2.py
+++ exercise2.py
@@ -1,2 +1,34 @@
+# Задайте список из n чисел последовательности (1 + 1/n)^n. и выведите на экран их сумму.
+
+def InputValues (text: str):
+    check = False
+    while not check:
+        try:
+            number = int(input(f'{text}'))
+            check = True
+        except ValueError:
+            number = int(input(f'{text}'))
+    return number
+
+# def orderOfNumbers(num):
+#     order = 0
+#     summ = 0
+#     listOfMulti = []
+#     for i in range(num):
+#         helpI = i+1
+#         order = (1 + 1/helpI)**helpI
+#         summ += order
+#         listOfMulti.append(f'{helpI}: {order}')
+#     defString = f'A set of order for {num} is {listOfMulti} and sum of numbers is {summ}'
+#     return defString
+    
+# numberN = InputValues('Enter number N: ')
+# print(orderOfNumbers(numberN))
+
+#-----------------------------------------------------------------------------------------
+
+numberN = InputValues('Enter number N: ')
+listOfNumbers = [(1 + 1/i)**i for i in range(1,numberN + 1)] 
+print(listOfNumbers)
 
 
